c9ed99a540be4e11f6c9b3703324ce685ab05042
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-29 16:30:29 +0530

    create express server

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const express = require('express');
+
 const bot = require('./services/bot');
 
-bot.launch();
+const app = express();
+
+app.use('/', (req, res) => {
+    res.status(200).json({
+        ok: true,
+        message: 'server running'
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Live on PORT:${process.env.PORT}`);
+    try {
+        bot.launch();
+        console.log(`@CompilexBot Online`);
+    } catch (err) {
+        console.error();
+    }
+});
+
+
