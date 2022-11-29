eca5dd420d909360cd0efa811f464a559031d177
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-29 11:56:30 +0530

    add help & build commands

--- services/bot.js
+++ services/bot.js
@@ -9,4 +9,12 @@ bot.command('start', async ctx => {
     ctx.reply(`This bot can help you build and run your source code\\. You can send the source file or type /help for more information\\.\n_Currently supports \\[C/C\\+\\+, Java, Python\\]_`, { parse_mode: 'MarkdownV2' })
 });
 
+bot.command('help', ctx => {
+    ctx.reply(`/start: start bot\n/help: need help\n/build: to build source coden\n/template: get templates for different languages`, { parse_mode: 'MarkdownV2' });
+});
+
+bot.command('build', ctx => {
+    ctx.reply(`Build under progress`);
+});
+
 module.exports = bot;
