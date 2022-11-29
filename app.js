86e4a628d8d1deb065d771c18e6a33ef3889752a
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-29 11:53:03 +0530

    import bot from service

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// @ComplilexBot [t.me/CompilexBot]
-
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.start(ctx => {
-    ctx.reply('Hi there 👋');
-});
+const bot = require('./services/bot');
 
 bot.launch();
